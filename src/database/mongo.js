84cf5f9f64f85c21deae2c5e2a84a9b93f14543d
Fernanda <99283312+nanda662@users.noreply.github.com>
2024-12-04 21:26:36 -0300

    Update mongo.js

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 async function connectDatabase() {
     await mongoose.connect(
-        'mongodb+srv://izaquemiranda881:cYYeF47sUQaMG2FN@adc-api.f4bal.mongodb.net/?retryWrites=true&w=majority&appName=ADC-api'
+        'mongodb+srv://izaquemiranda881:adc-api.f4bal.mongodb.net/?retryWrites=true&w=majority&appName=ADC-api'
     )
 
 }
