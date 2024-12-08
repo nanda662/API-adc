6cae14a2104df726811dce7ad2c9a9c3c8a81845
Fernanda <fernandagutierrez491@gmail.com>
2024-12-08 07:49:09 -0300

    yay

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const connectDatabase = async () => {
   const mongoUri = process.env.MONGO_URI; 
-  await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(mongoUri);
   console.log("MongoDB conectado!");
 };
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import User from "./models/User.js";
 import connectDatabase from "./database/mongo.js";
 import dotenv from 'dotenv';
 dotenv.config();
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
